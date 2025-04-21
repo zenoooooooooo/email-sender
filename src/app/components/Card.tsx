c07041868846572ac9af3c69cfe7feb94cdab3af
E-Jhay Esplana <esplanaejhaym@gmail.com>
2025-04-22 07:19:14 +0800

    fixed card ui inconsistent size

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -11,13 +11,11 @@ const Card: React.FC<CardProps> = ({ title, description, tags, link }) => {
   return (
     <Link
       href={link}
-      className="bg-gray-800 w-full max-w-sm h-[340px] flex flex-col justify-between text-white p-6 rounded-lg shadow-lg hover:bg-gray-900 duration-300 overflow-hidden"
+      className="bg-gray-800 w-full flex flex-col justify-between text-white p-6 rounded-lg shadow-lg hover:bg-gray-900 duration-300 overflow-hidden"
     >
       <div>
         <h3 className="text-xl font-semibold">{title}</h3>
-        <p className="mt-2 text-sm line-clamp-4">
-          {description}
-        </p>
+        <p className="mt-2 text-sm line-clamp-4">{description}</p>
       </div>
 
       <div className="mt-4 flex flex-wrap gap-2 max-h-[80px] overflow-y-auto pr-1">
